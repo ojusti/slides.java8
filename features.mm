ff37c41f135ea140efea1fae1ef2b9f8ea309fe1
Justinian Oprescu <ojusti@yahoo.fr>
2015-02-19 11:25:28 +0200

    initial mindmap

---
+++ features.mm
@@ -0,0 +1,22 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1424246017687" ID="ID_930862978" MODIFIED="1424246168810" TEXT="Features">
+<node CREATED="1424246152718" ID="ID_542612599" MODIFIED="1424246288208" POSITION="right" TEXT="Java 7">
+<node CREATED="1424246173655" ID="ID_831012677" LINK="http://docs.oracle.com/javase/7/docs/technotes/guides/language/underscores-literals.html" MODIFIED="1424246193026" TEXT="Underscores in Numeric Literals"/>
+<node CREATED="1424246213293" ID="ID_1367355087" LINK="http://docs.oracle.com/javase/7/docs/technotes/guides/language/strings-switch.html" MODIFIED="1424246235325" TEXT="Strings in switch Statements"/>
+<node CREATED="1424246292686" ID="ID_350085186" LINK="http://docs.oracle.com/javase/7/docs/technotes/guides/language/type-inference-generic-instance-creation.html" MODIFIED="1424246312122" TEXT="Type Inference for Generic Instance Creation"/>
+<node CREATED="1424246325366" ID="ID_1038357680" LINK="http://docs.oracle.com/javase/7/docs/technotes/guides/language/try-with-resources.html" MODIFIED="1424246337463" TEXT="The try-with-resources Statement"/>
+<node CREATED="1424246340404" ID="ID_1952972794" LINK="http://docs.oracle.com/javase/7/docs/technotes/guides/language/catch-multiple.html" MODIFIED="1424246361424" TEXT="Catching Multiple Exception Types and Rethrowing Exceptions with Improved Type Checking"/>
+</node>
+<node CREATED="1424246390852" ID="ID_906098515" MODIFIED="1424246572926" POSITION="left" TEXT="Java 8">
+<node CREATED="1424246400973" ID="ID_1937720204" LINK="http://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html" MODIFIED="1424246472805" TEXT="Lambda Expressions">
+<node CREATED="1424246953265" ID="ID_1409648596" MODIFIED="1424246956581" TEXT="Parallel"/>
+</node>
+<node CREATED="1424246533475" ID="ID_1855541113" LINK="http://docs.oracle.com/javase/tutorial/java/javaOO/methodreferences.html" MODIFIED="1424246548948" TEXT="Method References"/>
+<node CREATED="1424246573923" ID="ID_1976946421" LINK="http://docs.oracle.com/javase/tutorial/java/IandI/defaultmethods.html" MODIFIED="1424246582406" TEXT="Default Methods"/>
+<node CREATED="1424246619603" ID="ID_111622709" LINK="http://docs.oracle.com/javase/8/docs/technotes/guides/language/lambda_api_jdk8.html" MODIFIED="1424246701283" TEXT="Streams"/>
+<node COLOR="#ff0000" CREATED="1424246740954" ID="ID_1561007275" LINK="http://docs.oracle.com/javase/8/docs/technotes/guides/datetime/index.html" MODIFIED="1424263130611" TEXT="Date-Time Package"/>
+<node CREATED="1424246994544" ID="ID_1395094775" MODIFIED="1424247002413" TEXT="Removal of PermGen"/>
+</node>
+</node>
+</map>
